3e92e7045188b585cf51bc36301ccea1dac96793
Nelson <nelsonsprague84@gmail.com>
2019-07-02 17:13:04 -0600

    MVP MVP MVP!

--- bam.js
+++ bam.js
@@ -8,71 +8,87 @@ var connection = mysql.createConnection({
     port: 3307,
     database: "bamazon"
 })
-function queryAll(){
-    connection.query("SELECT *FROM products", function(err, res){
-        if(err) throw err;
+
+function queryAll() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
         console.table(res)
-        
-            var productIds = res.map(function (res){
-                return res.item_id
-            });
-            var stockQuantity = res.map(function (res){
-                return res.stock_quantity
-            });
+
+        var productIds = res.map(function (res) {
+            return res.item_id
+        });
+        var productCost = res.map(function(res){
+            return res.price
+        })
+        var stockQuantity = res.map(function (res) {
+            return res.stock_quantity
+        });
 
 
-userBuy(productIds, stockQuantity);
-});
+        userBuy(productIds);
+    });
 }
 
-connection.connect(function(err){
-    if(err)throw err;
+connection.connect(function (err) {
+    if (err) throw err;
     console.log("got itwoo! " + connection.threadId + "\n")
 })
 queryAll();
 
-function userBuy(productIds, stockQuantity){
+function userBuy(productIds) {
+
+
+    inquirer.prompt({
+            type: "list",
+            name: "productList",
+            message: "What product ID would you like to buy?",
+            choices: productIds
+        })
+        .then(function (answer) {
+            connection.query("SELECT * FROM products Where ?", {
+                item_id: answer.productList
+            }, function(err, response){
+                buyItem(response)
+                
+            })
+           
+        })
+}
+
+function buyItem(response) {
+    inquirer.prompt({
+            type: "input",
+            name: "purchaseQuantity",
+            message: "How many would you like to buy?"
+        })
+        .then(function (answer2) {  
+            console.log(response[0].stock_quantity)
+            if(parseInt(answer2.purchaseQuantity)<= parseInt(response[0].stock_quantity)){
+                completeTransaction(response, answer2);
+                queryAll();
+            }else{
+                console.log(`Insufficient quantity! We don't have that many ${response[0].product_name}(s)! You can't buy what we don't have!`);
+                buyItem(response);
+            }
+        })
+}
+
+function completeTransaction(response, answer2) {
     
-
-    inquirer.prompt({
-        type: "list",
-        name: "productList",
-        message: "What product ID would you like to buy?",
-        choices: productIds
-    })
-    .then(function(answer){
-        switch (answer.productList){
-           case answer.productList:
-            buyItem(stockQuantity);
-            break;
+    var remainingQuantity = parseInt(response[0].stock_quantity) - parseInt(answer2.purchaseQuantity);
+    console.log(remainingQuantity)
+    var totalCost = parseFloat(response[0].price) * parseFloat(answer2.purchaseQuantity)
+    console.log(`Your purchase of ${answer2.purchaseQuantity} ${response[0].product_name}(s) comes out to $ ${totalCost}. We appreciate your business!`)
+    connection.query(
+        "UPDATE products SET ? WHERE ?",[{
+            stock_quantity: remainingQuantity
+        },{
+            item_id: response[0].item_id
+        }],
+        function(error){
+            if(error) throw error;
         }
-    })
-}
-
-function buyItem(stockQuantity){
-    inquirer.prompt({
-        type: "input",
-        name: "purchaseQuantity",
-        message: "How many would you like to buy?"
-    })
-    .then(function(answer){
-        var query = "SELECT stock_quantity FROM products WHERE id = ?"
-        if(parseInt(answer.purchaseQuantity)===4){
-            completeTransaction();
-        }else{
-            console.log("Insufficient quantity! You can't buy what we don't have!");
-            buyItem();
-        }
-    })
-}
-
-function completeTransaction(){
-    console.log("here")
-            var remainingQuantity = parseInt(stockQuantity) - parseInt(answer.purchaseQuantity);
-            console.log(remainingQuantity)
-            var query = "UPDATE products SET stock_quantity = remainingQuantity WHERE productID = userID"
-           connection.query(query, {remainingQuantity: answer.stock_quantity}, function(err, res){
-               if (err) throw err;
-               queryAll(res);
-           })
+        );
+        console.log(`Products updated. We have ${remainingQuantity} left! Better buy before we run out!`);
+        queryAll();
 }
